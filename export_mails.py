9be66ec86d3c7915b8e17dcd84b7a4e76b9bd7c0
Benjamin Rain <tulak@hord.dev>
2024-03-19 08:31:39 +0200

    Update export_mails.py
    Changed completely to work with local outlook

--- export_mails.py
+++ export_mails.py
@@ -1,56 +1,49 @@
 import os
 import win32com.client
-import imaplib2
-import email
-
-# IMAP Connection function
-def connect_to_email_server(username, password):
-    mail = imaplib2.IMAP4_SSL('outlook.office365.com')
-    mail.login(username, password)
-    return mail
-
-# Extract emails from the cyber_il folder into an emails list, data[0] contains the metadata
-# data[0][1] contains the email content itself
-def get_emails(mail_instance, cyberil_folder):
-    mail_instance.select(cyberil_folder)
-    result, data = mail_instance.search(None, 'ALL')
-    email_ids = data[0].split()
-
-    emails = []
-    for email_id in email_ids:
-        result, data = mail_instance.fetch(email_id, '(RFC822)')
-        raw_email = data[0][1]
-        email_message = email.message_from_bytes(raw_email)
-        emails.append(email_message)
-
-    return emails
+import time
+import re
 
 
-def export_emails_to_keyword_folders(emails, output_folder):
+def export_emails_to_keyword_folders(folder, output_folder):
     counter = 1
-    for message in emails:
-        body = message.get_payload().lower() # Retrieves the content of the current email
-        if "ip" in body:
-            save_email_no_filter(output_folder, 'ip', message, counter)
-            # save_email_and_filter(output_folder, 'ip', message)
-            save_ip_tag(output_folder, 'ip', message)
-        elif "url" in body:
-            save_email_no_filter(output_folder, 'url', message, counter)
-            # save_email_and_filter(output_folder, 'url', message)
-            save_url_tag(output_folder, 'url', message)
-        elif "domain" in body:
-            save_email_no_filter(output_folder, 'domain', message, counter)
-            # save_email_and_filter(output_folder, 'domain', message)
-            save_domain_tag(output_folder, 'domain', message)
-        elif "md5" in body:
-            save_email_no_filter(output_folder, 'md5', message, counter)
-            # save_email_and_filter(output_folder, 'md5', message)
-        elif "sha256" in body:
-            save_email_no_filter(output_folder, 'sha256', message, counter)
-            # save_email_and_filter(output_folder, 'sha256', message)
-        # Reset body variable for the next iteration
-        body = None
-        counter += 1
+    pattern_ip = r'\bip\b'
+    pattern_domain = r'\bdomain\b'
+    pattern_url = r'\burl\b'
+    pattern_md5 = r'\bmd5\b'
+    pattern_sha256 = r'\bsha256\b'
+    pattern_sha1 = r'\bsha1\b'
+    pattern_domain_phishing = r'\b#Phishing\b'
+    for message in folder.Items:
+        if message.UnRead:
+            body = message.Body.lower()  # Retrieves the content of the current email
+            if re.search(pattern_ip, body):
+                save_email_no_filter(output_folder, 'ip', message, counter)
+                save_ip_tag(message)
+            elif re.search(pattern_domain and pattern_domain_phishing, body):
+                save_email_no_filter(output_folder, 'domain_phishing', message, counter)
+                save_domain_phishing_tag(message)
+            elif re.search(pattern_domain, body):
+                save_email_no_filter(output_folder, 'domain', message, counter)
+                save_domain_tag(message)
+            # URL Keyword saved separately
+            elif re.search(pattern_url, body):
+                save_email_no_filter(output_folder, 'url', message, counter)
+                save_url_tag(message)
+            # Hash keywords
+            elif re.search(pattern_md5, body):
+                save_email_no_filter(output_folder, 'md5', message, counter)
+                save_md5_tag(message)
+            elif re.search(pattern_sha256, body):
+                save_email_no_filter(output_folder, 'sha256', message, counter)
+                save_sha256_tag(message)
+            elif re.search(pattern_sha1, body):
+                save_email_no_filter(output_folder, 'sha1', message, counter)
+                save_sha1_tag(message)
+            # Mark as read after tagging the message
+            message.UnRead = False
+            # Reset body variable for the next iteration
+            body = None
+            counter += 1
 
 
 def save_email_no_filter(output_folder, keyword, message, counter):
@@ -72,59 +65,94 @@ def save_email_no_filter(output_folder, keyword, message, counter):
     with open(filepath, 'w', encoding='utf-8') as export_file:
         write_whole_email_to_txt(export_file, message)
 
-def save_ip_tag(output_folder, keyword, message):
-    txt_files_folder = r'C:\Users\Benjamin\Desktop\Work\Test'
+
+def save_ip_tag(message):
+    txt_files_folder = r'C:\Users\Kosta\Desktop\cyber.feed'
 
     if not os.path.exists(txt_files_folder):
         os.makedirs(txt_files_folder)
 
-    filename = f'exported_file.txt'  # Where to save the IPs
-    filepath = os.path.join(txt_files_folder, filename)
-
-    with open(filepath, 'a', encoding='utf-8') as export_file:
-        append_subject_to_file(export_file, message)
-def save_domain_tag(output_folder, keyword, message):
-    txt_files_folder = r'C:\Users\Benjamin\Desktop\Work\Test'
-
-    if not os.path.exists(txt_files_folder):
-        os.makedirs(txt_files_folder)
-
-    filename = f'exported_file.txt'  # Where to save the Domains
-    filepath = os.path.join(txt_files_folder, filename)
-
-    with open(filepath, 'a', encoding='utf-8') as export_file:
-        append_subject_to_file(export_file, message)
-def save_url_tag(output_folder, keyword, message):
-    txt_files_folder = r'C:\Users\Benjamin\Desktop\Work\Test'
-
-    if not os.path.exists(txt_files_folder):
-        os.makedirs(txt_files_folder)
-
-    filename = f'exported_file.txt'  # Where to save the urls
+    filename = f'feedlistip.txt'  # Where to save the IPs
     filepath = os.path.join(txt_files_folder, filename)
 
     with open(filepath, 'a', encoding='utf-8') as export_file:
         append_subject_to_file(export_file, message)
 
 
-# def save_email_and_filter(output_folder, keyword, message):
-#     keyword_with_filtered = keyword + '_filtered'
-#     filtered_folder = os.path.join(output_folder, 'filtered')
-#     keyword_folder = os.path.join(filtered_folder, keyword_with_filtered)
-#
-#     # Create the main "not_filtered" folder if it doesn't exist
-#     if not os.path.exists(filtered_folder):
-#         os.makedirs(filtered_folder)
-#
-#     # Create the keyword subfolder within the "not_filtered" folder
-#     if not os.path.exists(keyword_folder):
-#         os.makedirs(keyword_folder)
-#
-#     filename = f'exported_file_{keyword}.txt'  # Modify filename to include counter
-#     filepath = os.path.join(keyword_folder, filename)
-#
-#     with open(filepath, 'a', encoding='utf-8') as export_file:
-#         append_subject_to_file(export_file, message)
+def save_domain_tag(message):
+    txt_files_folder = r'C:\Users\Kosta\Desktop\cyber.feed'
+
+    if not os.path.exists(txt_files_folder):
+        os.makedirs(txt_files_folder)
+
+    filename = f'feedlistdomain.txt'  # Where to save the Domains
+    filepath = os.path.join(txt_files_folder, filename)
+
+    with open(filepath, 'a', encoding='utf-8') as export_file:
+        append_subject_to_file(export_file, message)
+
+
+def save_domain_phishing_tag(message):
+    txt_files_folder = r'C:\Users\Kosta\Desktop\cyber.feed'
+
+    if not os.path.exists(txt_files_folder):
+        os.makedirs(txt_files_folder)
+
+    filename = f'feedlistdomainPhishing.txt'  # Where to save the Domains
+    filepath = os.path.join(txt_files_folder, filename)
+
+    with open(filepath, 'a', encoding='utf-8') as export_file:
+        append_subject_to_file(export_file, message)
+
+
+def save_sha256_tag(message):
+    txt_files_folder = r'C:\Users\Kosta\Desktop\cyber.feed'
+
+    if not os.path.exists(txt_files_folder):
+        os.makedirs(txt_files_folder)
+
+    filename = f'feedlistsha256.txt'
+    filepath = os.path.join(txt_files_folder, filename)
+
+    with open(filepath, 'a', encoding='utf-8') as export_file:
+        append_subject_to_file(export_file, message)
+
+def save_sha1_tag(message):
+    txt_files_folder = r'C:\Users\Kosta\Desktop\cyber.feed'
+
+    if not os.path.exists(txt_files_folder):
+        os.makedirs(txt_files_folder)
+
+    filename = f'feedlistsha1.txt'
+    filepath = os.path.join(txt_files_folder, filename)
+
+    with open(filepath, 'a', encoding='utf-8') as export_file:
+        append_subject_to_file(export_file, message)
+
+def save_md5_tag(message):
+    txt_files_folder = r'C:\Users\Kosta\Desktop\cyber.feed'
+
+    if not os.path.exists(txt_files_folder):
+        os.makedirs(txt_files_folder)
+
+    filename = f'feedlistmd5.txt'
+    filepath = os.path.join(txt_files_folder, filename)
+
+    with open(filepath, 'a', encoding='utf-8') as export_file:
+        append_subject_to_file(export_file, message)
+
+
+def save_url_tag(message):
+    txt_files_folder = r'C:\Users\Kosta\Desktop\cyber.feed'
+
+    if not os.path.exists(txt_files_folder):
+        os.makedirs(txt_files_folder)
+
+    filename = f'feedlistURL.txt'  # Where to save the urls
+    filepath = os.path.join(txt_files_folder, filename)
+
+    with open(filepath, 'a', encoding='utf-8') as export_file:
+        append_subject_to_file(export_file, message)
 
 
 def append_subject_to_file(file, message):
@@ -139,16 +167,28 @@ def write_whole_email_to_txt(file, message):
 
 
 if __name__ == "__main__":
-    # Connection info
-    username = "monitor@itcare.co.il"
-    password = ""
-    mail = connect_to_email_server(username, password)
+    outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+    
+    # Iterate through top-level folders
+    for folder in outlook.Folders:
+    # Check if the folder is your main inbox
+        if folder.Name == "kosta@itcare.co.il":
+            # Access the 'cyberil.feed' folder within the main inbox
+            try:
+                cyberil_folder = folder.Folders['cyberil']
+                # If found, you can work with the cyberil_folder here
+                print("Found 'cyberil' folder!")
+            except KeyError:
+                print("The 'cyberil' folder was not found within the main inbox.")
+            break  # Exit loop after finding the main inbox
+    else:
+        print("Your main inbox was not found.")
+        
+    output_folder = r'C:\Users\Kosta\Desktop\cyber.feed'
 
-    cyberil_folder = "cyberil"
-    # outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-    # cyberil_folder = outlook.Folders[1].Folders['cyberil']
-    output_folder = r'C:\Users\Benjamin\Desktop\Work\Test'
-    emails = get_emails(mail, cyberil_folder)
-    export_emails_to_keyword_folders(emails, output_folder)
+    export_emails_to_keyword_folders(cyberil_folder, output_folder)
     print("Exported emails to respective keyword folders.")
+    time.sleep(3)  # Pause for 1 second
 
+    # Prompt the user to press Enter to continue
+    input("Press Enter to exit...")
